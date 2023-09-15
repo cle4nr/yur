77a24759f04651d0b6f58fabc712d806fe02a1ac
cle4nr <144906981+cle4nr@users.noreply.github.com>
2023-09-15 12:30:42 +0100

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,23 @@
+#task 4
+            #program 1 will type out "Leaving Certificate Computer Science"
+            #it wouldve printed what i expected
+
+#word1= "L"
+#word2 = "C"
+#word3 = "C"
+#word4 = "S"
+#subjectname = word1 + word2
+#print(subjectname)
+     
+
+#program 2
+
+#it will print the first 3 letters
+#pangram = "the quick brown fox jumps over the lazy dog!"
+#print (pangram[:3] + pangram[16:1])
+
+#program 3
+            #it will add an s
+
+#noun = input("enter a singular noun: ")
+#print("the plural of "+noun+" is "+noun+"s")
