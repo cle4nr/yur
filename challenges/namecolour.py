a1f72d1d1c4aaa497d054d0f14c9b463db6b7f56
cle4nr <144906981+cle4nr@users.noreply.github.com>
2023-11-15 12:09:30 +0000

    Add files via upload

---
+++ challenges/namecolour.py
@@ -0,0 +1,10 @@
+user_name = input("What is your name? ")
+fav_colour = input("What's your favourite colour? ")
+print("Hi",user_name,"your favourite colour is",fav_colour)
+print("Goodbye",user_name)
+
+""""
+variables allow you to easily store information to be re-used later in your code while using minimal amount of code
+it also lets you store user input to be later sent back to the user in a different format
+
+""""
