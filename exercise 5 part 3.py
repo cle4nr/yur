ef2a8d1096e96a4654a340821b26e63dca2bb0a3
cle4nr <144906981+cle4nr@users.noreply.github.com>
2023-11-15 12:09:37 +0000

    Add files via upload

---
+++ exercise 5 part 3.py
@@ -0,0 +1,16 @@
+daysleft = 167
+print("the variable daysleft is initialised to",daysleft)
+rate = 18.27
+rate = int(rate)
+print("the value 18.27 is stored in the variable called rate")
+name = "Alex"
+print("the value",name,"is assigned to the variable name")
+vowels = "AEIOUaeiou"
+print("the english vowels are",vowels,"assigned to the variable vowels")
+pwd = input("Password: ")
+print("the value entered by the user is stored in the variable pwd")
+hoursworked = 2
+hoursworked = int(hoursworked)
+pay = hoursworked*rate
+print("the value",rate,"is multiplied by the value",hoursworked,"and the result is stored in the variable pay")
+print("the pay is",pay,)
