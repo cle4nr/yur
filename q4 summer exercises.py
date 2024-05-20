977f75f415f566914415467b03e3d26b1f2c8032
cle4nr <144906981+cle4nr@users.noreply.github.com>
2024-05-20 11:54:53 +0100

    Add files via upload

---
+++ q4 summer exercises.py
@@ -0,0 +1,9 @@
+
+sellerlist = []
+salevalue = []
+print("*** WELCOME ***\n\nPlease choose an option below\n")
+which = input("1. Enter sales data\n2. View total sales to date\n3. View maximum sale value of any staff member\n4. View minimum sale of any staff member\n5. View average sale value of any staff member\n")
+if which = 1:
+    seller = input("Enter the name of the sales person:	")
+    value = input("Enter the value of the sale:		")
+    
